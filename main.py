9a73c9642910f8c65f931856fb71d3e715343939
lynnetsy <lyncelgarros@gmail.com>
2021-10-09 13:57:24 -0500

    Json wods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -6,7 +6,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hellooooou miamibitch'
+    return 'Index page'
+
+
+@app.route('/random-wod')
+def randomwod_index():
+    return jsonify({
+        'title': 'Wod del 9 de octubre',
+        'description': 'mortal legs cardio',
+        'name': None,
+        'type': 'for_time',
+        'time': 900,
+        'rounds': 3,
+        'exercises': [
+            {
+                'name': 'burpee',
+                'reps': 20,
+            },
+            {
+                'name': 'clean',
+                'reps': 10,
+                'weight': 90,
+                'unit': 'lbs',
+        
+            }
+
+        ]
+    }), 200
 
 
 if __name__ == '__main__':
